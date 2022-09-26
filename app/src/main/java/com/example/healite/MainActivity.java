d4fbe9b71580d9c0629dbca3a230f9831df9fb91
vcuencas <vcuencas@ufl.edu>
2022-09-25 21:29:17 -0400

    Updated User Interface, register user, set up google firebase

--- app/src/main/java/com/example/healite/MainActivity.java
+++ app/src/main/java/com/example/healite/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.healite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //FirebaseFirestore firestore;
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+
+//        firestore = FirebaseFirestore.getInstance();
+//
+//        Map<String,String> users = new HashMap<String,String>();
+//
+//        users.put("firstName", "EASY");
+//        users.put("lastName", "TUTO");
+//        users.put("description", "Subscribe");
+//
+//        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//            @Override
+//            public void onSuccess(DocumentReference documentReference) {
+//                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
+//            }
+//        });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
