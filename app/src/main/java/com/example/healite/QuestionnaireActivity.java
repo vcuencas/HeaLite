4a255ba78c01ed2446fb6c633b55ce52ccad8e1d
Salome Asatiani <61980344+Salteki@users.noreply.github.com>
2022-11-04 18:58:50 -0400

    Connected submit button to user profileActivity

--- app/src/main/java/com/example/healite/QuestionnaireActivity.java
+++ app/src/main/java/com/example/healite/QuestionnaireActivity.java
@@ -2,6 +2,7 @@ package com.example.healite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,8 +12,12 @@ public class QuestionnaireActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
+//        startActivity(new Intent(this, ProfileActivity.class));
     }
 
     public void submitbuttonHandler(View view) {
+        if (view.getId() == R.id.submitButton) {
+            startActivity(new Intent(this, ProfileActivity.class));
+        }
+        }
     }
-}
