2b007a6bb2e444aaa0625fb3e789db54237e0b94
vcuencas <vcuencas@ufl.edu>
2022-11-30 23:50:18 -0500

    Journal entry is being displayed per date clicked.

--- app/src/main/java/com/example/healite/CalendarFragment.java
+++ app/src/main/java/com/example/healite/CalendarFragment.java
@@ -6,16 +6,43 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.healite.Model.JournalEntry;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CalendarFragment extends Fragment {
 
-    private TextView dateSelected;
+    private TextView dateSelected, moodRate, dayRate, description;
+    //private TextView text;
     private CalendarView calendar;
+    private DatabaseReference reference;
+    private FirebaseUser user;
+    private String userID;
+    private ListView listView;
+    private String date, dayString, moodString, generalString;
+    private ArrayList<JournalEntry> notesList = new ArrayList<>();
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -26,6 +53,7 @@ public class CalendarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false);
+
     }
 
     @Override
@@ -34,14 +62,52 @@ public class CalendarFragment extends Fragment {
 
         dateSelected = view.findViewById(R.id.dateSelected);
         calendar = view.findViewById(R.id.full_calendar);
+        moodRate = view.findViewById(R.id.textView_moodRate);
+        dayRate = view.findViewById(R.id.textView_dayRate);
+        description = view.findViewById(R.id.textView_general);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Entry Journal");
+        userID = user.getUid();
+
+        reference.child(userID).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    for (DataSnapshot ds : task.getResult().getChildren()) {
+                        int dRate = ds.child("dayRate").getValue(Integer.class);
+                        int mRate = ds.child("moodRate").getValue(Integer.class);
+                        String general = ds.child("general").getValue(String.class);
+                        String noteDate = ds.child("todayDate").getValue(String.class);
+
+                        JournalEntry note = new JournalEntry(noteDate, dRate, mRate, general);
+                        notesList.add(note);
+                    }
+                }
+            }
+        });
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
-                String date = dayOfMonth+"/"+month+"/"+year;
+                date = (month + 1) + "-" + dayOfMonth + "-" + year;
                 dateSelected.setText(date);
+
+                for (JournalEntry currentNote : notesList) {
+                    if (currentNote.getTodayDate().compareTo(date) == 0) {
+                        dayString = "Day Rate: " + currentNote.getDayRate();
+                        moodString = "Mood Rate: " + currentNote.getMoodRate();
+                        generalString = "Description: " + currentNote.getGeneral();
+                    }
+                }
+
+                dayRate.setText(dayString);
+                moodRate.setText(moodString);
+                description.setText(generalString);
             }
         });
-
     }
 }
